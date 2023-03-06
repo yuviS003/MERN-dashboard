bdc1ff58d46072d892b4a235978d418175ad0740
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-06 10:23:51 +0530

    backend setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World" });
+});
+
+const startServer = async () => {
+  try {
+    // connect to the database
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => {
+      console.log(`Server started on http://localhost:8080`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+startServer();
