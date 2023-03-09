85d9112f2b97ee04d68a89c1905a4ff5ff46033c
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-09 18:49:12 +0530

    agent-profile

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useOne } from "@pankod/refine-core";
+import { useParams } from "@pankod/refine-react-router-v6";
+
+import { Profile } from "components";
 
 const AgentProfile = () => {
-  return <div>agentProfile</div>;
+  const { id } = useParams();
+  const { data, isLoading, isError } = useOne({
+    resource: "users",
+    id: id as string,
+  });
+
+  const agentProfile = data?.data ?? [];
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>Error...</div>;
+
+  return (
+    <Profile
+      type="Agent"
+      name={agentProfile.name}
+      email={agentProfile.email}
+      avatar={agentProfile.avatar}
+      properties={agentProfile.allProperties}
+    />
+  );
 };
 
 export default AgentProfile;
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -29,8 +29,7 @@ const getUserInfoByID = async (req, res) => {
     const { id } = req.params;
     const user = await User.findOne({ _id: id }).populate("allProperties");
     if (user) return res.status(200).json(user);
-
-    return res.status(404).json({ message: "User not found" });
+    else return res.status(404).json({ message: "User not found" });
   } catch (e) {
     console.log(e);
     res.status(500).json({ message: e.message });
