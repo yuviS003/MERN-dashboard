5db664e90ddcbbcf3b85b75bb441a3a55a708e2c
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-09 19:35:58 +0530

    project complete

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -9,8 +9,22 @@ import {
   PropertyCard,
   TopAgent,
 } from "../components";
+import { useList } from "@pankod/refine-core";
 
-const home = () => {
+const Home = () => {
+  const { data, isLoading, isError } = useList({
+    resource: "properties",
+    config: {
+      pagination: {
+        pageSize: 5,
+      },
+    },
+  });
+
+  const latestProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Error</Typography>;
   return (
     <Box>
       <Typography fontSize={25} fontWeight={700} color="#11142d">
@@ -57,8 +71,34 @@ const home = () => {
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
+      <Box
+        flex={1}
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fcfcfc"
+        display="flex"
+        flexDirection="column"
+        minWidth="100%"
+        mt="25px"
+      >
+        <Typography fontSize="18px" fontWeight={600} color="#11142d">
+          Latest Properties
+        </Typography>
+        <Box mt={2.5} sx={{ display: "flex", flexWrap: "wrap", gap: 4 }}>
+          {latestProperties.map((property) => (
+            <PropertyCard
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              location={property.location}
+              price={property.price}
+              photo={property.photo}
+            />
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-export default home;
+export default Home;
